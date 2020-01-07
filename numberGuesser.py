7aa7e1eb581ba58c775f3561978c6e7e0b52a921
Will Short <short.will@gmail.com>
2020-01-07 12:50:05 -0800

    •Updated data validation for integers outside the given range •Updated loop parameters to ask for replay
    Known Issues:
    •Validation for replay input
    •Script could be smaller and cleaner

--- numberGuesser.py
+++ numberGuesser.py
@@ -1,22 +1,28 @@
 import random
 
-
-secretNumber = random.randint(1,20)
-guessNumber = 0
-
-def guessResult(guessNumber):
+# main loop of guessing game
+def guessResult():
+    secretNumber = random.randint(1, 20)
+    guessNumber = 0
     while True:
         guessNumber += 1
-        guess = int(input("I'm thinking of a number between 1 and 20. What is your guess?"))
+        guess = int(input("\nI'm thinking of a number between 1 and 20. What is your guess? \nTYPE A NUMBER: "))
         if guess == secretNumber:
             print("Congratulations, your guess of " + str(guess) + " is correct. \n You got the correct answer in " + str(guessNumber) + " guesses \n")
-            break
-        elif guess < secretNumber:
-            print("Your guess is too low, try again")
-            guessResult(guessNumber)
-        elif guess > secretNumber:
-            print("Your Guess is too high, try again")
-            guessResult(guessNumber)
+            guessNumber = 0
+            retry = str(input("Would you like to play again? yes/no \n"))
+            if retry == str('yes'):
+                guessResult()
+            else:
+                print("Goodbye")
+                break
+        elif 0 < guess < secretNumber:
+            print("Your guess is too low, try again \n")
+        elif 21 > guess > secretNumber:
+            print("Your Guess is too high, try again \n")
+        else:
+            print("Invalid choice, please select and integer between 1-20 \n")
 
-guessResult(guessNumber)
+
+guessResult()
 
